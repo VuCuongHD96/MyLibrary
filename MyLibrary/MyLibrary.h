f1e1ff66139a2d330f2535b4e899c1fcb815e877
cuongvx <sun@Suns-Mac-mini.local>
2021-11-02 11:33:32 +0700

    base project

---
+++ MyLibrary/MyLibrary.h
@@ -0,0 +1,18 @@
+//
+//  MyLibrary.h
+//  MyLibrary
+//
+//  Created by Sun on 02/11/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyLibrary.
+FOUNDATION_EXPORT double MyLibraryVersionNumber;
+
+//! Project version string for MyLibrary.
+FOUNDATION_EXPORT const unsigned char MyLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyLibrary/PublicHeader.h>
+
+
